7cc3ddc2dd193826f653ace2a08651aac77ae038
Rana Emad <rana3emad@gmail.com>
2018-12-16 22:21:00 +0200

    Set routes

---
+++ config/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+$routes["GET"]["get_all"]="get_all";
+$routes["GET"]["get_one"]="get_one";
+$routes["POST"]["create"]="create";
+$routes["PUT"]["update"]="update";
+$routes["DELETE"]["delete"]="delete";
