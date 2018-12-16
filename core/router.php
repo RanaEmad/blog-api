cf7b78f5c2c9646d8e2cc35f0e292f1d52213c88
Rana Emad <rana3emad@gmail.com>
2018-12-16 22:21:54 +0200

    Basic router

---
+++ core/router.php
@@ -0,0 +1,21 @@
+<?php
+require 'config/routes.php';
+class Router{
+    private $routes;
+    public function __construct($routes) {
+        $this->routes=$routes;
+    }
+    public function get_method(){
+        $request_method=$_SERVER["REQUEST_METHOD"];
+        $url = explode('/', trim($_SERVER["REQUEST_URI"]));
+        $method="";
+        if(count($url)>2){
+            $method=$url[2];
+            if(!empty($this->routes[$request_method]) && !empty($this->routes[$request_method][$method])){
+                return $this->routes[$request_method][$method];
+            }
+        }
+        return FALSE;
+        
+    }
+}
