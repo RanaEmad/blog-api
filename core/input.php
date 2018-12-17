a76fb96d44826cefefb98950a8ddf6963033f1b3
Rana Emad <rana3emad@gmail.com>
2018-12-17 23:39:58 +0200

    Input validator

---
+++ core/input.php
@@ -0,0 +1,56 @@
+<?php
+class Input{
+    protected $field;
+    protected $error;
+    public function __construct() {
+        $this->error="";
+    }
+
+    public function post($field, $validations=NULL){
+        $response["result"]=TRUE;
+        $this->field=$field;
+        $value= htmlspecialchars(trim($_POST[$field]));
+        if($validations){
+            foreach ($validations as $one){
+                $valid=$this->{$one}($value);
+                if(!$valid){
+                    $response["result"]=FALSE;
+                    $response["error"]=  $this->error;
+                    return $response;
+                }
+            }
+        }
+        $response["value"]=$value;
+        return $response;
+    }
+    public function get($field, $validations=NULL){
+        $response["result"]=TRUE;
+        $this->field=$field;
+        $value= htmlspecialchars(trim($_GET[$field]));
+        if($validations){
+            foreach ($validations as $one){
+                $valid=$this->{$one}($value);
+                if(!$valid){
+                    $response["result"]=FALSE;
+                    $response["error"]=  $this->error;
+                    return $response;
+                }
+            }
+        }
+        $response["value"]=$value;
+        return $response;
+    }
+    public function required($value){
+        if($value && $value!=""){
+            return TRUE;
+        }
+        return FALSE;
+    }
+    public function numeric($value){
+        if(is_numeric($value)){
+            return TRUE;
+        }
+        return FALSE;
+    }
+    
+}
